53231548cf64f98ec8b346b0ba0b71676fbed196
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2013-12-29 08:45:46 -0800

    More instructions

--- README.md
+++ README.md
@@ -18,8 +18,18 @@ Type ``create-java-project [project-name]`` where [project-name] is the name you
 This will:
 - copy skeleton files
 - substitute the string PROJECTNAME to the project name both inside files and in the name of files/folders
+- setup a git repository and submodules for frequently modified libs
 - run ``gradle eclipse`` to prepare the folder to be imported by eclipse
 - create a script (name upon the project name) to run the java program
+
+Then from eclipse:
+- ``Import`` in ``File`` menue
+- ``Import existing projects into workspace``
+- Select the root
+- Deselect ``Copy projects into workspace`` to avoid having duplicates
+
+Finally, type ``gradle tutorialj`` from the root of the repository to create a basic
+``README.md`` with further instructions.
 
 
 Customization
--- src/main/java/PROJECTNAME/Main.java
+++ src/main/java/PROJECTNAME/Main.java
@@ -8,6 +8,23 @@ import com.beust.jcommander.Parameter;
 
 /**
  * # PROJECTNAME
+ * 
+ * Installing from source
+ * ----------------------
+ * 
+ * Requires: gradle, git, eclipse
+ * 
+ * - Clone the repository
+ * - Type ``gradle eclipse`` from the root of the repository
+ * - From eclipse:
+ *   - ``Import`` in ``File`` menue
+ *   - ``Import existing projects into workspace``
+ *   - Select the root
+ *   - Deselect ``Copy projects into workspace`` to avoid having duplicates
+ * - You can call PROJECTNAME.Main from the command line via ``./PROJECTNAME``
+ *   - Move the executable ``PROJECTNAME`` to a PATH folder (or add this folder to PATH)
+ *   to have access from anywhere
+ *   - Changes done via eclipse will be reflected right away
  */
 @Tutorial(startTutorial = "README.md")
 public class Main implements Runnable

07997086c35558e9c97a3421280b252297bbe3a4
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2013-12-29 08:11:42 -0800

    Change frequently access projects to git submodule instead of maven

--- src/main/java/PROJECTNAME/Main.java
+++ src/main/java/PROJECTNAME/Main.java
@@ -1,8 +1,25 @@
 package PROJECTNAME;
 
-public class Main
+import static briefj.CommandLineUtils.*;
+import static briefj.BriefIO.*;
+
+import com.beust.jcommander.Parameter;
+
+public class Main implements Runnable
 {
+  @Parameter(names = "--input", required = true)
+  private String input;
+  
   public static void main(String [] args)
   {
+    start(new Main(), args);
+  }
+
+  @Override
+  public void run()
+  {
+    System.out.println("Hello world");
+    for (String line : readLines(input))
+      System.out.println(line);
   }
 }

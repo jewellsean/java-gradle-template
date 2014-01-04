7065e3f9a048cafab9f552b23c939a1b06ffa5c6
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-01-04 11:20:39 -0800

    Upgrade tutorialj

--- src/main/java/PROJECTNAME/Main.java
+++ src/main/java/PROJECTNAME/Main.java
@@ -6,27 +6,7 @@ import tutorialj.Tutorial;
 
 import com.beust.jcommander.Parameter;
 
-/**
- * # PROJECTNAME
- * 
- * Installing from source
- * ----------------------
- * 
- * Requires: gradle, git, eclipse
- * 
- * - Clone the repository
- * - Type ``gradle eclipse`` from the root of the repository
- * - From eclipse:
- *   - ``Import`` in ``File`` menu
- *   - ``Import existing projects into workspace``
- *   - Select the root
- *   - Deselect ``Copy projects into workspace`` to avoid having duplicates
- * - You can call PROJECTNAME.Main from the command line via ``./PROJECTNAME``
- *   - Move the executable ``PROJECTNAME`` to a PATH folder (or add this folder to PATH)
- *   to have access from anywhere
- *   - Changes done via eclipse will be reflected right away
- */
-@Tutorial(startTutorial = "README.md")
+
 public class Main implements Runnable
 {
   @Parameter(names = "--input", required = true)
@@ -37,6 +17,27 @@ public class Main implements Runnable
     start(new Main(), args);
   }
 
+  /**
+   * # PROJECTNAME
+   * 
+   * Installing from source
+   * ----------------------
+   * 
+   * Requires: gradle, git, eclipse
+   * 
+   * - Clone the repository
+   * - Type ``gradle eclipse`` from the root of the repository
+   * - From eclipse:
+   *   - ``Import`` in ``File`` menu
+   *   - ``Import existing projects into workspace``
+   *   - Select the root of the newly created repo
+   *   - Deselect ``Copy projects into workspace`` to avoid having duplicates
+   * - You can call PROJECTNAME.Main from the command line via ``./PROJECTNAME``
+   *   - Move the executable ``PROJECTNAME`` to a PATH folder (or add this folder to PATH)
+   *   to have access from anywhere
+   *   - Changes done via eclipse will be reflected right away
+   */
+  @Tutorial(startTutorial = "README.md", showSource = false)
   @Override
   public void run()
   {
